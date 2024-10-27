51c6a01a9b466f3c4db09382ca449d5006edcd1d
Paavo Hietanen <paavo.hietanen@protonmail.com>
2024-10-27 12:39:58 +0200

    Successfully reads from SQS

--- telemetry_processor/src/main.rs
+++ telemetry_processor/src/main.rs
@@ -1,7 +1,11 @@
+use aws_config::imds::client;
+use aws_config::BehaviorVersion;
+use aws_sdk_kinesis::config;
+use aws_sdk_sqs::types::Message;
 use aws_sdk_sqs::{Client, Config, config::Region};
 use std::sync::Arc;
 use tokio::{runtime::Runtime, sync::Semaphore};
-use tokio::task;
+use tokio::{task, time};
 use tokio_stream::StreamExt;
 
 
@@ -14,33 +18,48 @@ use tokio_stream::StreamExt;
 #[derive(Clone)]
 struct SubmissionWorker {
     client: Client,
-    queue_url: String,
 }
 
 impl SubmissionWorker {
-    pub fn new(client: Client, queue_url: String) -> Self {
-        Self { client, queue_url }
+    pub fn new(client: Client) -> Self {
+        Self { client }
     }
 
     // Process submissions function
-    pub async fn process_submission(&self) {
+    pub async fn process_submission(&self, submission: &Message) {
         // Code for reading and processing from SQS here
+        // Read the message contents and log them
+        if let Some(body) = &submission.body {
+            println!("Received message: {}", body);
+        }
     }
 
     // Main loop for submission processing
     pub async fn run(self) {
+
+        // Polling loop with individual task spawn for each submission
         loop {
-            // Polling loop with individual task spawn for each submission
-            if let Ok(msg) = self.client.receive_message().queue_url(&self.queue_url).send().await {
-                if let Some(messages) = msg.messages {
-                    for message in messages {
-                        let processor = self.clone();
-                        task::spawn(async move {
-                            processor.process_submission().await;
-                        });
+            // Read a submission from SQS, spawn a task to process it or log an error
+            let result = match self
+                .client
+                .receive_message()
+                .send()
+                .await {
+                Ok(submission) => {
+                    if let Some(messages) = submission.messages {
+                        for message in messages {
+                            let processor = self.clone();
+                            task::spawn(async move {
+                                processor.process_submission(&message).await;
+                            });
+                        }
                     }
+                    time::sleep(time::Duration::from_secs(1)).await;
+                },
+                Err(error) => {
+                    println!("Error receiving messages: {:?}", error);
                 }
-            }
+            };
         }
     }
 }
@@ -48,11 +67,25 @@ impl SubmissionWorker {
 
 #[tokio::main]
 async fn main() {
-    let region = Region::new("us-west-2"); // Adjust as necessary
-    let config = aws_config::load_from_env().await;
-    let client = Client::new(&config);
-    let queue_url = "http://localstack:4566".to_string();
-    let processor = SubmissionWorker::new(client, queue_url);
+    // Build the configuration that we want to use
+    let config = aws_sdk_sqs::config::Builder::new()
+        .region(Region::new("eu-west-1"))
+        .endpoint_url("http://localhost:4566/000000000000/submissions") // Queue URL: Differs from 'list-queues' URL, that one gives error
+        .credentials_provider(aws_sdk_sqs::config::Credentials::new(
+            "some_key_id",  // Access key for LocalStack
+            "some_secret",  // Secret key for LocalStack
+            None,               // Optional session token
+            None,               // Expiry (optional)
+            "localstack",       // Provider name
+        ))
+        .build();
 
-    processor.run().await; // Start the processing loop
+    // Construct the client based on our configuration
+    let client = aws_sdk_sqs::Client::from_conf(config);
+
+    // Create a new SubmissionWorker instance
+    let worker = SubmissionWorker::new(client);
+
+    // Run the worker
+    worker.run().await;
 }
