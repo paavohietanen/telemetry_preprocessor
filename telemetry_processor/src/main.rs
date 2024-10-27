1b9b584606f9f44259889c7795af51f09176bc5f
Paavo Hietanen <paavo.hietanen@protonmail.com>
2024-10-27 13:24:14 +0200

    Deserialization of submissions with some logging

--- telemetry_processor/src/main.rs
+++ telemetry_processor/src/main.rs
@@ -3,7 +3,9 @@ use aws_config::BehaviorVersion;
 use aws_sdk_kinesis::config;
 use aws_sdk_sqs::types::Message;
 use aws_sdk_sqs::{Client, Config, config::Region};
+use base64::{self, Engine, engine::general_purpose};
 use serde::{Serialize, Deserialize};
+use tokio::time::sleep;
 use std::sync::Arc;
 use tokio::{runtime::Runtime, sync::Semaphore};
 use tokio::{task, time};
@@ -47,6 +49,11 @@ struct NetworkConnection {
     destination_port: u16,
 }
 
+// Helper function to decode base64 binary data
+fn decode_base64(encoded_data: &str) -> Result<Vec<u8>, base64::DecodeError> {
+    general_purpose::STANDARD.decode(encoded_data)
+}
+
 // Handles
 // - Reading of submissions from the SQS
 // - Validating and processing them
@@ -65,10 +72,40 @@ impl SubmissionWorker {
 
     // Process submissions function
     pub async fn process_submission(&self, submission: &Message) {
-        // Code for reading and processing from SQS here
-        // Read the message contents and log them
         if let Some(body) = &submission.body {
-            println!("Received message: {}", body);
+            // Decode the base64 encoded submission
+            let decoded_data =  match decode_base64(body) {
+                Ok(decoded_data) => { 
+                    println!("Decoded successfully: {:?}", decoded_data);
+                    decoded_data
+                },
+                Err(error) => {
+                    println!("Error decoding submission: {:?}", error);
+                    return;
+                }
+            };
+            // Convert the decoded data to a string
+            let submission_str = match String::from_utf8(decoded_data) {
+                Ok(submission_str) => {
+                    println!("Submission string: {:?}", submission_str);
+                    submission_str
+                },
+                Err(error) => {
+                    println!("Error converting submission to string: {:?}", error);
+                    return;
+                }
+            };
+            // Deserialize the json string to a Submission struct
+            let submission: Submission = match serde_json::from_str(&submission_str) {
+                Ok(submission) => {
+                    println!("Deserialized submission: {:?}", submission);
+                    submission
+                },
+                Err(error) => {
+                    println!("Error deserializing submission: {:?}", error);
+                    return;
+                }
+            };
         }
     }
 
@@ -86,13 +123,15 @@ impl SubmissionWorker {
                 Ok(submission) => {
                     if let Some(messages) = submission.messages {
                         for message in messages {
+                            println!("Received message: {:?}", message);
                             let processor = self.clone();
                             task::spawn(async move {
                                 processor.process_submission(&message).await;
                             });
                         }
                     }
-                    time::sleep(time::Duration::from_secs(1)).await;
+                    // sleep for one second
+                    sleep(time::Duration::from_secs(1)).await;
                 },
                 Err(error) => {
                     println!("Error receiving messages: {:?}", error);
